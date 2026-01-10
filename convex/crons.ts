2def5f9166374f883b26082c993dd6ac52bfef50
ITMusumba <itmusumba@gmail.com>
2026-01-11 00:22:57 +0300

    fix: Temporarily disable cron job until scheduled module is registered

--- convex/crons.ts
+++ convex/crons.ts
@@ -2,19 +2,23 @@
  * Scheduled Cron Jobs
  * 
  * Defines scheduled functions that run automatically on a schedule.
+ * 
+ * NOTE: Currently disabled until scheduled module is properly registered.
+ * To enable, ensure scheduled.ts is properly imported in the Convex API.
  */
 
 import { cronJobs } from "convex/server";
-import { internal } from "./_generated/api";
+// import { internal } from "./_generated/api";
 
 const crons = cronJobs();
 
 // Run expired UTID check every 5 minutes
 // This checks for UTIDs that are overdue by 1 hour and automatically expires them
-crons.interval(
-  "check expired UTIDs",
-  { minutes: 5 }, // Run every 5 minutes
-  internal.scheduled.checkExpiredUTIDs,
-);
+// TODO: Re-enable once scheduled module is properly registered
+// crons.interval(
+//   "check expired UTIDs",
+//   { minutes: 5 }, // Run every 5 minutes
+//   internal.scheduled.checkExpiredUTIDs,
+// );
 
 export default crons;

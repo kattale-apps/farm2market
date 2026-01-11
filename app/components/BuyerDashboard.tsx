d5da44edc18d5db0089944acb052e822134d8627
ITMusumba <itmusumba@gmail.com>
2026-01-11 09:14:25 +0300

    Fix missing NotificationMailbox import in BuyerDashboard

--- app/components/BuyerDashboard.tsx
+++ app/components/BuyerDashboard.tsx
@@ -6,6 +6,7 @@ import { Id } from "../../convex/_generated/dataModel";
 import { useState } from "react";
 import { exportToExcel, exportToPDF, formatUTIDDataForExport } from "../utils/exportUtils";
 import { formatUgandaDateTime, getUgandaTime } from "../utils/timeUtils";
+import { NotificationMailbox } from "./NotificationMailbox";
 
 interface BuyerDashboardProps {
   userId: Id<"users">;

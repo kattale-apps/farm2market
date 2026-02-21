bf224d50657bf3f31bf313b6b3d870c2cf42e981
itmusumba <itmusumba@gmail.com>
2026-02-22 00:58:43 +0300

    cleanup: Remove test file export

--- convex/index.ts
+++ convex/index.ts
@@ -28,7 +28,6 @@ export * from "./pdfGeneration";
 export * from "./pilotMode";
 export * from "./pilotSetup";
 export * from "./seedUgandaLocations";
-export * from "./testJuniorAdminCreation";
 export * from "./userSettings";
 export * from "./utils";
 export * from "./wallet";
